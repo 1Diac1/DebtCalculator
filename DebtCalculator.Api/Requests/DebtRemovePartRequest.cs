bd499752c3679fe29676c29232641303ac6e6176
Izotikov <izotikov.igor1@yandex.ru>
2022-04-17 23:28:19 +0300

    Merge branch 'master' of https://github.com/1Diac1/DebtCalculator

--- DebtCalculator.Api/Requests/DebtRemovePartRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DebtCalculator.Api.Requests
-{
-    public class DebtRemovePartRequest
-    {
-        public int UserId { get; set; }
-        public int DebtId { get; set; }
-        public decimal Amount { get; set; }
-    }
-}
