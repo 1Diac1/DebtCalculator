9d3c6bee557b313e70ad73c4f37e0ff5407f2bef
Ruslan <ruslan>
2022-04-17 18:37:06 +0300

    add some data

--- DebtCalculator.DAL/Data/ApplicationDbContextSeed.cs
+++ DebtCalculator.DAL/Data/ApplicationDbContextSeed.cs
@@ -1,6 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 using DebtCalculator.Core.Models;
-using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System;
 
 namespace DebtCalculator.DAL.Data
 {
@@ -8,6 +9,37 @@ namespace DebtCalculator.DAL.Data
     {
         public static async Task SeedSampleDataAsync(ApplicationDbContext context)
         {
+            if (!await context.Debts.AnyAsync())
+            {
+                await context.AddRangeAsync(
+                    new Debt
+                    {
+                        Name = "Задолженность 1",
+                        Description = "Какое-то дополнение",
+                        Amount = 12000,
+                        Created = DateTime.Now.ToString("G"),
+                        UserId = 1,
+                        CreditorId = 2
+                    }, new Debt
+                    {
+                        Name = "Задолженность 2",
+                        Description = "Какое-то дополнение 2",
+                        Amount = 123123123,
+                        Created = DateTime.Now.ToString("G"),
+                        UserId = 1,
+                        CreditorId = 2
+                    },
+                    new Debt
+                    {
+                        Name = "Задолженность 3",
+                        Description = "Какое-то дополнение 3",
+                        Amount = 123123123,
+                        Created = DateTime.Now.ToString("G"),
+                        UserId = 1,
+                        CreditorId = 2
+                    });
+            }
+
             if (!await context.Users.AnyAsync())
             {
                 await context.AddRangeAsync
